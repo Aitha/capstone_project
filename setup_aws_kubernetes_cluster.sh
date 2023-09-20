f14dc484f9a738d94c8aa24c3e8b3e2c12517868
Satish Kumar <sateeshfrnd@users.noreply.github.com>
2023-09-20 11:30:08 +0530

    added files for Kubernetes cluster

---
+++ setup_aws_kubernetes_cluster.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+count=$(grep -i eksctl-udacity-devops-capstone-project output.txt | wc -l)
+echo '$count'
+
+
+if [[ $count -gt 0 ]]
+    then 
+    echo "Cluster already exists"
+    else
+    echo "Creatiing AWS Kubernetes cluster"
+    eksctl create cluster \
+        --name udacity-devops-capstone-project \
+        --version 1.19 \
+        --region $AWS_DEFAULT_REGION \
+        --nodegroup-name udacity-devops-capstone-project \
+        --node-type t3.large \
+        --nodes-min 2 \
+        --nodes-max 4 \
+        --managed
+fi
